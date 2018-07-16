1d4cfd3f059d44cf864b796a9a9bc25f6085a8f2
Ahmed Bafadal <a.m.bafadal@gmail.com>
2018-07-16 21:41:33 +0100

    Added capability to scrape next page.

--- craigslist/spiders/jobs.py
+++ craigslist/spiders/jobs.py
@@ -5,7 +5,28 @@ import scrapy
 class JobsSpider(scrapy.Spider):
     name = 'jobs'
     allowed_domains = ['london.craigslist.org']
-    start_urls = ['http://london.craigslist.org/']
+    start_urls = ['https://london.craigslist.co.uk/search/edu/']
 
     def parse(self, response):
-        pass
+        listings = response.xpath(
+            '//li[@class="result-row"]')
+        for listing in listings:
+            # Extracting the date
+            date = listing.xpath(
+                './/*[@class="result-date"]/@datetime').extract_first()
+            # Extracting the link from each posting
+            link = listing.xpath(
+                './/a[@class="result-title hdrlnk"]/@href').extract_first()
+
+            # Extracting the title from each posting
+            text = listing.xpath(
+                './/a[@class="result-title hdrlnk"]/text()').extract_first()
+
+            yield {'date': date,
+                   'link': link,
+                   'text': text,
+                   }
+        next_page_url = response.xpath(
+            '//a[text()="next > "]/@href').extract_first()
+        if next_page_url:
+            yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse)
