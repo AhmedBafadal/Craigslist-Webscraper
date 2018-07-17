520009df2c4be74dd6945aa6f2a0b5b3bb71c723
Ahmed Bafadal <a.m.bafadal@gmail.com>
2018-07-17 14:11:52 +0100

    Changed starting url to NY.

--- craigslist/spiders/jobs.py
+++ craigslist/spiders/jobs.py
@@ -1,32 +1,55 @@
+
 # -*- coding: utf-8 -*-
 import scrapy
 
 
 class JobsSpider(scrapy.Spider):
-    name = 'jobs'
-    allowed_domains = ['london.craigslist.org']
-    start_urls = ['https://london.craigslist.co.uk/search/edu/']
+    name = "jobs"
+    allowed_domains = ["newyork.craigslist.org"]
+    start_urls = (
+        'https://newyork.craigslist.org/search/egr',
+    )
 
     def parse(self, response):
-        listings = response.xpath(
-            '//li[@class="result-row"]')
+        listings = response.xpath('//li[@class="result-row"]')
         for listing in listings:
-            # Extracting the date
             date = listing.xpath(
                 './/*[@class="result-date"]/@datetime').extract_first()
-            # Extracting the link from each posting
             link = listing.xpath(
                 './/a[@class="result-title hdrlnk"]/@href').extract_first()
-
-            # Extracting the title from each posting
             text = listing.xpath(
                 './/a[@class="result-title hdrlnk"]/text()').extract_first()
 
-            yield {'date': date,
-                   'link': link,
-                   'text': text,
-                   }
+            yield scrapy.Request(link,
+                                 callback=self.parse_listing,
+                                 meta={'date': date,
+                                       'link': link,
+                                       'text': text})
+
         next_page_url = response.xpath(
             '//a[text()="next > "]/@href').extract_first()
         if next_page_url:
             yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse)
+
+    def parse_listing(self, response):
+        date = response.meta['date']
+        link = response.meta['link']
+        text = response.meta['text']
+
+        compensation = response.xpath(
+            '//*[@class="attrgroup"]/span[1]/b/text()').extract_first()
+        type = response.xpath(
+            '//*[@class="attrgroup"]/span[2]/b/text()').extract_first()
+
+        images = response.xpath('//*[@id="thumbs"]//@src').extract()
+        images = [image.replace('50x50c', '600x450') for image in images]
+
+        address = response.xpath('//*[@id="postingbody"]/text()').extract()
+
+        yield {'date': date,
+               'link': link,
+               'text': text,
+               'compensation': compensation,
+               'type': type,
+               'images': images,
+               'address': address}
